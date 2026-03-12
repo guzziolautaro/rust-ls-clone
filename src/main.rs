20166f9949a0eef39afc6de70250c1691c624a7e
Lautaro Valentino Guzzio <lauguzzio@gmail.com>
2026-03-12 05:50:42 -0300

    implement DirEntry struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
 use std::io::{self, Write};
-use std::{env, fs, process};
+use std::{env, fs, process, path::Path};
+use std::os::unix::fs::MetadataExt;
 
 use color_print::cprint;
+
+enum FileType {
+    File,
+    ExecutableFile,
+    CompressedFile,
+    Directory,
+    SymLink,
+}
+
+struct DirEntry {
+    file_type: FileType,
+    name: String,
+    link_count: u64,
+    owner: String,
+    group: String,
+    size: u64,
+}
+
+impl DirEntry {
+    fn new(path: &str) -> DirEntry {
+        let metadata = fs::metadata(path).expect("invalid path");
+
+        let dir_entry = DirEntry {
+            file_type: {
+                if metadata.is_dir() {
+                    FileType::Directory
+                } else {
+                    FileType::File
+                }
+            },
+            name: match Path::new(path).file_name() {
+                Some(s) => {
+                    s.to_string_lossy().to_string()
+                }
+                None => String::new()
+            },
+            link_count: metadata.nlink(),
+            owner: String::from("root"), // todo
+            group: String::from("root"), // todo
+            size: metadata.size()
+        };
+        dir_entry
+    }
+}
 
 fn main() {
     let args = env::args().skip(1);
@@ -10,6 +55,7 @@ fn main() {
     let (flags, paths): (Vec<String>, Vec<String>) = args.into_iter().partition(|a| a.starts_with('-'));
 
     let mut show_hidden = false;
+
     // flag identifying
     for flag in &flags {
         match flag.as_str() {
@@ -25,16 +71,20 @@ fn main() {
 
     // defaulting to ./ if no path provided
     if paths.len() == 0 {
-        display_contents( "./", show_hidden);
+        display_direntry( "./", show_hidden);
     } else {
         for path in &paths {
             print!("{}:\n", path);
-            display_contents(path, show_hidden);
+            display_direntry(path, show_hidden);
         }
     }
 }
 
-fn display_contents(path: &str, show_hidden: bool) {
+fn get_entries() {
+    // todo
+}
+
+fn display_direntry(path: &str, show_hidden: bool) {
     let entries = fs::read_dir(path).unwrap();
     if show_hidden {
         cprint!("<blue><bold>. .. ")
