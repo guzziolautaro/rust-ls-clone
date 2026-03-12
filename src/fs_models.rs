ed563008bd1747a11cc742bc32f8567c9a12ded6
Lautaro Valentino Guzzio <lauguzzio@gmail.com>
2026-03-12 08:27:14 -0300

    move DirEntry to fs_models

---
+++ src/fs_models.rs
@@ -0,0 +1,49 @@
+use std::path::Path;
+use std::os::unix::fs::MetadataExt;
+
+pub enum FileType {
+    File,
+    ExecutableFile,
+    CompressedFile,
+    Directory,
+    SymLink,
+}
+
+pub struct DirEntry {
+    pub file_type: FileType,
+    pub name: String,
+    pub link_count: u64,
+    pub owner: String,
+    pub group: String,
+    pub size: u64,
+    pub hidden: bool
+}
+
+impl DirEntry {
+    pub fn new(path: &Path) -> DirEntry {
+        let metadata = path.metadata().unwrap();
+        let entry_name = match path.file_name() {
+            Some(s) => {
+                s.to_string_lossy().to_string()
+            }
+            None => String::new()
+        };
+
+        let dir_entry = DirEntry {
+            file_type: {
+                if metadata.is_dir() {
+                    FileType::Directory
+                } else {
+                    FileType::File
+                }
+            },
+            name: String::from(&entry_name),
+            link_count: metadata.nlink(),
+            owner: String::from("root"), // todo
+            group: String::from("root"), // todo
+            size: metadata.size(),
+            hidden: if entry_name.starts_with(".") { true } else { false }
+        };
+        dir_entry
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,9 @@
 use std::io::{self, Write};
-use std::{env, fs, process, path::Path};
-use std::os::unix::fs::MetadataExt;
+use std::{env, fs, process};
 
 use color_print::cprint;
 
-enum FileType {
-    File,
-    ExecutableFile,
-    CompressedFile,
-    Directory,
-    SymLink,
-}
-
-struct DirEntry {
-    file_type: FileType,
-    name: String,
-    link_count: u64,
-    owner: String,
-    group: String,
-    size: u64,
-    hidden: bool
-}
-
-impl DirEntry {
-    fn new(path: &Path) -> DirEntry {
-        let metadata = path.metadata().unwrap();
-        let entry_name = match path.file_name() {
-            Some(s) => {
-                s.to_string_lossy().to_string()
-            }
-            None => String::new()
-        };
-
-        let dir_entry = DirEntry {
-            file_type: {
-                if metadata.is_dir() {
-                    FileType::Directory
-                } else {
-                    FileType::File
-                }
-            },
-            name: String::from(&entry_name),
-            link_count: metadata.nlink(),
-            owner: String::from("root"), // todo
-            group: String::from("root"), // todo
-            size: metadata.size(),
-            hidden: if entry_name.starts_with(".") { true } else { false }
-        };
-        dir_entry
-    }
-}
+mod fs_models;
 
 fn main() {
     let args = env::args().skip(1);
@@ -85,17 +39,17 @@ fn main() {
     }
 }
 
-fn get_entries_from_dir(path: &str) -> Vec<DirEntry> {
-    let mut entries: Vec<DirEntry> = Vec::new();
+fn get_entries_from_dir(path: &str) -> Vec<fs_models::DirEntry> {
+    let mut entries: Vec<fs_models::DirEntry> = Vec::new();
 
     for dir in fs::read_dir(path).expect("invalid path") {
-        entries.push(DirEntry::new(dir.unwrap().path().as_path()));
+        entries.push(fs_models::DirEntry::new(dir.unwrap().path().as_path()));
     } 
 
     entries
 }
 
-fn display_dir_entries(entries: Vec<DirEntry>, show_hidden: bool) {
+fn display_dir_entries(entries: Vec<fs_models::DirEntry>, show_hidden: bool) {
     for entry in entries {
         
         let file_name = entry.name;
@@ -103,11 +57,11 @@ fn display_dir_entries(entries: Vec<DirEntry>, show_hidden: bool) {
         if !show_hidden && entry.hidden { continue; }
         
         match entry.file_type {
-            FileType::File => cprint!("{} ", &file_name),
-            FileType::ExecutableFile => cprint!("<green><bold>{} ", &file_name),
-            FileType::CompressedFile => cprint!("<red><bold>{} ", &file_name),
-            FileType::Directory => cprint!("<blue><bold>{} ", &file_name),
-            FileType::SymLink => cprint!("<cyan><bold>{} ", &file_name)
+            fs_models::FileType::File => cprint!("{} ", &file_name),
+            fs_models::FileType::ExecutableFile => cprint!("<green><bold>{} ", &file_name),
+            fs_models::FileType::CompressedFile => cprint!("<red><bold>{} ", &file_name),
+            fs_models::FileType::Directory => cprint!("<blue><bold>{} ", &file_name),
+            fs_models::FileType::SymLink => cprint!("<cyan><bold>{} ", &file_name)
         }
     }
     print!("\n");
