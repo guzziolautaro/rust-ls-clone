ad6573f68edcab23d2ba8ba9cb722a74e3924d5e
Lautaro Valentino Guzzio <lauguzzio@gmail.com>
2026-03-16 12:52:11 -0300

    wip implement -l/-lh flag

--- src/fs_models.rs
+++ src/fs_models.rs
@@ -1,7 +1,10 @@
 use std::fs;
-use std::os::unix::fs::MetadataExt;
+use std::os::unix::fs::{MetadataExt, PermissionsExt};
 use std::path::Path;
+use std::time::SystemTime;
+use chrono::{DateTime, Local};
 
+#[derive(PartialEq)]
 pub enum FileType {
     File,
     ExecutableFile,
@@ -17,6 +20,8 @@ pub struct DirEntry {
     pub owner: String,
     pub group: String,
     pub size: u64,
+    pub mod_time: SystemTime,
+    pub permissions: u32,
     pub hidden: bool,
 }
 
@@ -42,9 +47,31 @@ impl DirEntry {
             owner: String::from("root"), // todo
             group: String::from("root"), // todo
             size: metadata.size(),
+            mod_time: metadata.modified().unwrap(),
+            permissions: metadata.permissions().mode(),
             hidden: entry_name.starts_with("."),
         };
         dir_entry
+    }
+    pub fn mod_time(&self) -> String {
+        let datetime: DateTime<Local> = self.mod_time.into();
+        datetime.format("%b %d %H:%M").to_string()
+    }
+    pub fn permissions(&self) -> String {
+        let user = (self.permissions >> 6) & 0o7;
+        let group = (self.permissions >> 3) & 0o7;
+        let other = self.permissions & 0o7;
+
+        let to_rwx = |val| {
+            format!(
+                "{}{}{}",
+                if val & 4 != 0 { "r" } else { "-" },
+                if val & 2 != 0 { "w" } else { "-" },
+                if val & 1 != 0 { "x" } else { "-" }
+            )
+        };
+
+        format!("{}{}{}", to_rwx(user), to_rwx(group), to_rwx(other))
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::{self, Write};
 use std::{env, process};
 
-use color_print::cprint;
+use color_print::{cprint, cformat};
+
+use crate::fs_models::FileType;
 
 mod fs_models;
 
@@ -12,12 +14,24 @@ fn main() {
     let (flags, paths): (Vec<String>, Vec<String>) =
         args.into_iter().partition(|a| a.starts_with('-'));
 
+    // default to ./ if no path provided
+    let paths = if paths.is_empty() {
+        vec![String::from("./")]
+    } else { paths };
+
     let mut show_hidden = false;
+    let mut show_list = false;
 
     // flag identifying
     flags.iter().for_each(|flag| match flag.as_str() {
         "-a" => {
             show_hidden = true;
+        }
+        "-l" => {
+            show_list = true;
+        }
+        "-lh" => {
+            show_list = true;
         }
         _ => {
             eprintln!("Invalid flag {}", flag);
@@ -25,15 +39,22 @@ fn main() {
         }
     });
 
-    // defaulting to ./ if no path provided
-    if paths.len() == 0 {
-        let directory = fs_models::Directory::new("./");
-        display_dir_entries(directory.entries, show_hidden);
+    if paths.len() == 1 {
+        let directory = fs_models::Directory::new(&paths[0]);
+        if !show_list {
+            display_dir_entries(directory.entries, show_hidden);
+        } else {
+            display_list_dir_entries(directory.entries, show_hidden);
+        }
     } else {
         for path in &paths {
             let directory = fs_models::Directory::new(path);
-            print!("{}:\n", path);
-            display_dir_entries(directory.entries, show_hidden);
+            println!("{}:", path);
+            if !show_list {
+                display_dir_entries(directory.entries, show_hidden);
+            } else {
+                display_list_dir_entries(directory.entries, show_hidden);
+            }
         }
     }
 }
@@ -48,6 +69,33 @@ fn display_dir_entries(entries: Vec<fs_models::DirEntry>, show_hidden: bool) {
             fs_models::FileType::CompressedFile => cprint!("<red><bold>{} ", &f.name),
             fs_models::FileType::Directory => cprint!("<blue><bold>{} ", &f.name),
             fs_models::FileType::SymLink => cprint!("<cyan><bold>{} ", &f.name),
+        });
+    print!("\n");
+    io::stdout().flush().unwrap();
+}
+
+fn display_list_dir_entries(entries: Vec<fs_models::DirEntry>, show_hidden: bool) {
+    entries
+        .iter()
+        .filter(|f| !(!show_hidden && f.hidden))
+        .for_each(|f| {
+            let display_file = format!("{dir}{permissions} {n_links} {user} {group} {size} {mod_time} {name} \n",
+                dir = if f.file_type == FileType::Directory { "d" } else { "-" },
+                permissions = f.permissions(),
+                n_links = f.link_count,
+                user = f.owner,
+                group = f.group,
+                size = f.size,
+                mod_time = f.mod_time(),
+                name = match f.file_type {
+                    fs_models::FileType::File => cformat!("{} ", &f.name),
+                    fs_models::FileType::ExecutableFile => cformat!("<green><bold>{} ", &f.name),
+                    fs_models::FileType::CompressedFile => cformat!("<red><bold>{} ", &f.name),
+                    fs_models::FileType::Directory => cformat!("<blue><bold>{} ", &f.name),
+                    fs_models::FileType::SymLink => cformat!("<cyan><bold>{} ", &f.name),
+                }
+            );
+            print!("{}", display_file);
         });
     print!("\n");
     io::stdout().flush().unwrap();
